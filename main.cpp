653b93305b88c8b89cb6d65f2e06151448bbefec
ErenVarli9 <eren.varli@epitech.eu>
2025-05-06 13:56:11 +0200

    Add initial implementation of a C++ program to greet user and determine age category

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <stdio.h>
+
+const char* sentence(unsigned short age) {
+    if (age <= 10 )
+        return "jeune !";
+    else if (age > 10 && age < 16) 
+        return "grand, mais t'es encore ado..";
+    else 
+        return "tu es grand.";
+}
+
+int main(){
+    char nom[15];
+    unsigned short age;
+
+    std::cout << "Bonjour, quel est votre nom ? " << std::endl;
+    std::cout << "- ";
+    std::cin >> nom;
+    std::cout << "Bonjour " << nom << " !" << std::endl;
+    std::cout << "Quel age as-tu ?" << std::endl;
+    std::cout << "- ";
+    std::cin >> age;
+    std::cout << "Wow " << nom << ", tu as " << age << " ans, tu es assez " << sentence(age) << std::endl;
+
+    return 0;
+}
